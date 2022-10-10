132662bfeea412b07ca7a480279c20fb3e11ee94
Mark <81118976+Markun-In@users.noreply.github.com>
2022-10-10 15:28:13 +0300

    Add files via upload

---
+++ Planner/Planner/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup.Localizer;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Planner.Models;
+using Planner.Services;
+
+namespace Planner
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<TodoModel> _toDoDataList;
+        private FileIOService _fileIOService;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                _toDoDataList = _fileIOService.LoadData();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                Close();
+            }
+
+            dgTodoList.ItemsSource = _toDoDataList;
+            _toDoDataList.ListChanged += _toDoDataList_ListChanged;
+
+        }
+
+        private void _toDoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded ||
+                e.ListChangedType == ListChangedType.ItemDeleted ||
+                e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                    Close();
+                }
+            }
+        }
+    }
+}
